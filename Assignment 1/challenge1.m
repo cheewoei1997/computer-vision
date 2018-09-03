832389f934a71151ca90fc5414c488cf8a9315e4
cheewoei1997 <cheewoei1997@gmail.com>
2018-09-03 08:24:11 +0800

    Challenge 3

--- Assignment 1/challenge1.m
+++ Assignment 1/challenge1.m
@@ -8,11 +8,10 @@ imgclear = imread('ultrasoundclear.jpg');
 subplot(2, 2, 2)
 imshow(imgclear, []), title('Desired image')
 
- LEN = 10; THETA = 30;
+LEN = 10; THETA = 30;
 motion_noise = fspecial('motion', LEN, THETA);
 
 wnr = deconvwnr(img, motion_noise, 0.01);   % deblur using wiener filter
 luri = deconvlucy(img, motion_noise);       % deblur using lucy-richardson
 subplot(2, 2, 3), imshow(wnr), title('Deblur using Wiener');
 subplot(2, 2, 4), imshow(luri), title('Deblur using Lucy-Richardson');
-
--- Assignment 1/challenge3.m
+++ Assignment 1/challenge3.m
@@ -1,20 +1,58 @@
-I = imread('images/plantation.jpg');
-I = rgb2gray(I)
-figure, imshow(I)
+img = imread('images/plantation.jpg');
+cform = makecform('srgb2lab');
+J = applycform(img,cform);
+figure;imshow(J),title('Convert to LAB color space');
 
-% Use Morphological Opening to Estimate the Background
-background = imopen(I,strel('disk',15));
-figure, imshow(background)
+% Equalise brightness to get skin area
+K = J(:,:,2);     % 2nd page of 3-d vector j
+figure;imshow(K),title('2nd axis of 3D vector');
 
-% Subtract the Background Image from the Original Image
-I2 = I - background;
-figure, imshow(I2)
+L=graythresh(J(:,:,2));     % find appropriate gray thresh value
+BW1=im2bw(J(:,:,2),L);      % convert to binary image based on threshold
+figure;imshow(BW1),title('Binary Image');
 
-% Increase the Image Contrast
-I3 = imadjust(I2);
-figure, imshow(I3);
+% Test 2 (better)
+se = strel('sphere',3);
+BW1 = imerode(BW1,se);
+figure;imshow(BW1),title('Image: Erosion 1');
 
-% Threshold the Image
-level = graythresh(I3);
-bw = im2bw(I3,level);
-figure, imshow(bw)
+se3 = strel('sphere',13);
+BW1 = imdilate(BW1,se3);
+figure;imshow(BW1),title('Image: Dilate');
+
+se = strel('sphere',5)
+BW1 = imerode(BW1,se);
+figure;imshow(BW1),title('Image: Erosion 2');
+% End test
+
+bw3 = bwareaopen(BW1,1890);     % opens area greater than 1890
+cc=bwconncomp(bw3)              % connected comp for finding the density 
+                                % of trees in image
+density=cc.NumObjects / (size(bw3,1) * size(bw3,2))
+figure;imshow(bw3),title('Removes all connected components')
+bw3 = imcomplement(bw3);
+figure;imshow(bw3),title('Complement')
+bw4=imfill(bw3,'holes');        % fill patches with holes
+
+labeledImage = bwlabel(bw4, 8);     % same as connected components
+blobMeasurements = regionprops(labeledImage,'all'); % measure all properties of
+                                                    % the image
+numberOfTree = size(blobMeasurements, 1)    % count the number of trees
+
+imagesc(img),title('Image with scaled colors');
+hold on;
+
+% draw bounding circles
+XY = [blobMeasurements.Centroid];
+
+%Find out the coordinates of labeled objects
+coordinate(:,1) = XY(1:2:end);
+coordinate(:,2) = XY(2:2:end);
+
+%Find out the radius of labeled objects
+radius(1:length(coordinate),:) = 15;
+
+%Create the circle
+figure, imshow(img), title('Final Result');
+h = viscircles(coordinate,radius);
+
