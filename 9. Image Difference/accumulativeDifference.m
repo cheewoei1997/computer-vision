206d3920913ec4a4a4ec7ae4f5d58a1a1f03ddfe
Phuah Chee Woei <cheewoei1997@gmail.com>
2018-09-07 23:29:08 +0800

    Lab 9

---
+++ 9. Image Difference/accumulativeDifference.m
@@ -0,0 +1,23 @@
+video = VideoReader('video.avi');
+nFrames = video.NumberOfFrames;
+[warning, Fs] = audioread('somethingwrong.wav');
+flag = 1;
+
+for k =2: nFrames-1
+    img1 = read(video,k-1);
+    img2 = read(video,k);
+    diff = abs(rgb2gray(img2) - (rgb2gray(img1)));
+    
+    if sum(sum(diff)) < 46000
+        imshow(img2, [])
+        drawnow
+    else
+        if flag
+            sound(warning, Fs)
+            flag = 0;
+        end
+        imshow(img2,[])
+        text(100,100,'Intruder!!!','FontSize',24)
+        drawnow
+    end
+end
